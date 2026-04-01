57dd18398b71085605a50e4a584a9142538a337d
Sagor <mahamudshagor93@gmail.com>
2026-04-02 02:34:01 +0600

    updated project

--- src/components/portfolio/Projects.tsx
+++ src/components/portfolio/Projects.tsx
@@ -1,13 +1,12 @@
+import { ExternalLink, Github } from "lucide-react";
 import { motion, useInView } from "framer-motion";
 import { useRef } from "react";
-import { ExternalLink, Github } from "lucide-react";
-import { Link } from "react-router-dom";
 
 const projects = [
   {
     projectUrl: "https://themeforest.net/item/agntix-digital-agency-creative-portfolio-nextjs-template/59844674?s_rank=4",
     title: "Agntix – Digital Agency & Portfolio",
-    image: "/public/projects/agntix_preview-image.png",
+    image: "/projects/agntix_preview-image.png",
     desc: "A Modern and Responsive Agency & Portfolio Template built with Next.js",
     tags: ["Next.js", "TypeScript", "GSAP", "Boostrap"],
     color: "from-blue-500/20 to-violet-500/20",
@@ -16,7 +15,7 @@ const projects = [
   {
     projectUrl: "https://themeforest.net/item/orgado-ecommerce-next-template/48140546?s_rank=19",
     title: "Orgado - Organic Food eCommerce Next Template + Admin Panel",
-    image: "/public/projects/orgado.jpg",
+    image: "/projects/orgado.jpg",
     desc: "Orgado is a modern agriculture-focused e-commerce platform built with React, Next.js, TypeScript, Node.js, and Express.",
     tags: ["Next.js", "Mongodb", "Express js", "Mongose", "Stripe"],
     color: "from-emerald-500/20 to-teal-500/20",
@@ -25,7 +24,7 @@ const projects = [
   {
     projectUrl: "https://themeforest.net/item/hrm-crm-next-js-dashboard-template-manez/55890159?s_rank=5",
     title: "Manez – HRM & CRM Dashboard",
-    image: "/public/projects/manez_preview_preview.png",
+    image: "/projects/manez_preview_preview.png",
     desc: "A Complete HRM & CRM Admin Dashboard Template built with Next.js",
     tags: ["Next.js", "Typescript", "Redux", "Tailwind CSS"],
     color: "from-amber-500/20 to-orange-500/20",
@@ -34,7 +33,7 @@ const projects = [
   {
     projectUrl: "https://trx-gold-front-end.vercel.app/",
     title: "Money Hunt - investment site",
-    image: "/public/projects/trx_gold.jpg",
+    image: "/projects/trx_gold.jpg",
     desc: "Built a full-stack crypto platform with profit tracking, referrals, and admin management.",
     tags: ["Node.js", "Next.js", "Express js", "Mongodb"],
     color: "from-rose-500/20 to-pink-500/20",
